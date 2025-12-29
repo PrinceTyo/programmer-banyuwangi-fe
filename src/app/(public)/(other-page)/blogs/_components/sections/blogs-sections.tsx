eeb99216fc081f71b3eebd3f5543330a11160374
PrinceTyo <kresnaale2@gmail.com>
2025-12-29 10:35:57 +0700

    fix: overflow-y

--- src/app/(public)/(other-page)/blogs/_components/sections/blogs-sections.tsx
+++ src/app/(public)/(other-page)/blogs/_components/sections/blogs-sections.tsx
@@ -19,7 +19,7 @@ export default function BlogsSection() {
 
   return (
     <div
-      className="relative min-h-screen bg-black overflow-x-hidden py-20 px-2 md:px-8 lg:px-14"
+      className="relative min-h-screen bg-black overflow-x-hidden py-20 px-2 md:px-8 lg:px-14 overflow-y-hidden"
       style={{
         backgroundImage: `url("data:image/svg+xml,%3Csvg width='180' height='180' viewBox='0 0 180 180' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cpath d='M60 54L60 66M54 60L66 60' stroke='%236B7280' stroke-width='0.5' stroke-opacity='0.7'/%3E%3C/g%3E%3C/svg%3E"), 
         url("data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cpath d='M15 15L15 0M15 15L30 15M15 15L15 30M15 15L0 15' stroke='%231F2937' stroke-width='0.5' stroke-opacity='0.6'/%3E%3C/g%3E%3C/svg%3E")`,
